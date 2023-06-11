3ee65ce788e56d2a4070f7971527039a4dded55c
chaocauminhlason <95207985+chaocauminhlason@users.noreply.github.com>
2023-06-11 23:29:00 +0700

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,17 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void BubbleSort(int a[], int n){
-    int t; // biến tạm t
-    for (int i = 0; i < n; i++){
-	for (int j = i + 1; j < n; j++){
-		if (a[j] > a[j+1]){
-                    t = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = t;
-			}
-		}
-	}
-}
+
 // sắp sếp nổi bọt gaimr dần
 void bubbleSort(int a[], int n) {
     for (int i = 0; i < n - 1; i++) {
@@ -47,6 +36,55 @@ void mergeSort(int ){
     int m=(l+r)/2;
 
 }
+/*int partition( int a[], int l, int r){
+    int pivot = a[l];
+    int last = l ;
+   
+    for (int i=l+1,i <= r,i++){
+        if(a[last]<pivot){
+        i++;
+        swap(a[last],a[i]);
+    }
+    }
+    swap(a[l],a[last]);
+    return last;
+}*/
+void quickSort(int a[], int l, int r){
+    if(l >=r ) // base case
+        return;
+        int pivot = partition(a,l,r);
+    quickSort(a,l,pivot-1);
+    quickSort(a,pivot+1,r);
+}
+
+
+
+int tongChuSo(int a)
+{
+	int nRem, nSum = 0;
+	for (; a != 0;)
+	{
+		nRem = a[] % 10;
+		nSum = nSum + nRem;
+		a = a / 10;
+	}
+	return nSum;
+}
+    //bài tap
+// int partition( int a[], int l, int r){
+//     int pivot = a[l];
+//     int last = l ;
+   
+//     for (int i=l+1,i <= r,i++){
+//         tongChuSo(a[last]);
+//         if(nSum<pivot){
+//         i++;
+//         swap(a[last],a[i]);
+//     }
+//     }
+//     swap(a[l],a[last]);
+//     return last;
+};
 int main(){
 
     return 0;
